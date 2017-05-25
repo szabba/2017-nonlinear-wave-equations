a624fae0cfe8c2af66e483966d5d710b2ec55e2f
Karol Marcjan <karol.marcjan@gmail.com>
2017-05-25 14:47:27 +0200

    Sketch out a leap-frog integrator

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+func main() {
+}
+
+type LeapFrog struct {
+	Dom              *Domain
+	Next, Curr, Prev State
+	F_t              func(i int) float64
+	Δx               float64
+}
+
+func (leap *LeapFrog) Step() {
+	leap.Next = leap.Dom.New(func(i int) float64 {
+		return leap.Prev.At(i) + 2*leap.Δx*leap.F_t(i)
+	})
+	leap.Curr, leap.Prev = leap.Next, leap.Curr
+}
+
+type Domain struct {
+	size int
+}
+
+type State struct {
+	dom  *Domain
+	data []float64
+}
+
+func (dom *Domain) New(f func(i int) float64) State {
+	st := State{dom, make([]float64, dom.size)}
+	for i := range st.data {
+		st.data[i] = f(i)
+	}
+	return st
+}
+
+func (dom *Domain) Wrap(i int) int {
+	for i < 0 {
+		i += dom.size
+	}
+	for i >= dom.size {
+		i -= dom.size
+	}
+	return i
+}
+
+func (st *State) At(i int) float64 {
+	return st.data[st.dom.Wrap(i)]
+}
